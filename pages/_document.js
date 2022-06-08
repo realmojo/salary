5c12a642a05d101ff7a65a5d7373a19363862ecb
ㅇㅇ <strikers1999@naver.com>
2022-06-09 00:16:46 +0900

    add

--- pages/_document.js
+++ pages/_document.js
@@ -7,6 +7,11 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <script
+            async
+            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9130836798889522"
+            crossOrigin="anonymous"
+          />
           {/* Global Site Tag (gtag.js) - Google Analytics */}
           <script
             async
