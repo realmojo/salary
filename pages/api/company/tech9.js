d9163b964d186022bae127e89c7d3c1ffac2a762
realmojo88 <strikers1999@naver.com>
2022-06-10 10:05:22 +0900

    id update

--- pages/api/company/tech9.js
+++ pages/api/company/tech9.js
@@ -7,16 +7,13 @@ const handler = async (req, res) => {
     case "GET":
       const items = await Company.find({
         $or: [
-          { _id: 186418 },
-          { _id: 361729 },
-          { _id: 217130 },
-          { _id: 511600 },
-          { _id: 355281 },
-          { _id: 337008 },
-          { _id: 57336 },
-          { _id: 360680 },
-          { _id: 86159 },
-          { _id: 213795 },
+          { _id: 453873 },
+          { _id: 163935 },
+          { _id: 318781 },
+          { _id: 190408 },
+          { _id: 312501 },
+          { _id: 294789 },
+          { _id: 50195 },
         ],
       }).sort({ yearSalary: -1 });
       res.status(200).json(items);
